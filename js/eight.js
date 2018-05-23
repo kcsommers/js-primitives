4505cb7e102301dcf2a93788d81874ecfea59434
Mark Sommers <kacysommers@gmail.com>
2018-05-23 15:47:50 -0700

    added ten js files

---
+++ js/eight.js
@@ -0,0 +1,26 @@
+var foods = [
+  'Popcorn',
+  'Potato chips',
+  'Shrimp',
+  'Chicken rice',
+  'Poutine',
+  'Tacos',
+  'Toast',
+  'French Toast',
+  'Crab',
+  'Pho',
+  'Lasagna',
+  'Brownie',
+  'Lobster',
+  'Donuts',
+  'Ice cream',
+  'Hamburger',
+  'Sushi',
+  'Chocolate',
+  'Pizza'
+];
+
+var fifthFave = foods[4];
+var tenthFave = foods[9];
+
+console.log(fifthFave, tenthFave);
